d2905180a9c74ff73ad49d339469ce1bbe490af0
Bharathi-raja-pbr <76565518+Bharathi-raja-pbr@users.noreply.github.com>
2023-07-19 22:20:15 +0530

    Create Set.symmetric_difference().py

---
+++ Sets/Set.symmetric_difference().py
@@ -0,0 +1,41 @@
+
+'''
+Task
+Students of District College have subscriptions to English and French newspapers.
+Some students have subscribed to English only, some have subscribed to French only, and some have subscribed to both newspapers.
+
+You are given two sets of student roll numbers. One set has subscribed to the English newspaper, and one set has subscribed to the French newspaper.
+Your task is to find the total number of students who have subscribed to either the English or the French newspaper but not both
+
+Input Format
+
+The first line contains an integer,n , the number of students who have subscribed to the English newspaper.
+The second line contains n space separated roll numbers of those students.
+The third line contains ,b the number of students who have subscribed to the French newspaper.
+The fourth line contains b space separated roll numbers of those students.
+
+Constraints
+0< total no of students<1000
+
+Output Format
+
+Output the total number of students who have at least one subscription.
+
+Sample Input
+
+9
+1 2 3 4 5 6 7 8 9
+9
+10 1 2 3 11 21 55 6 8
+Sample Output
+
+8
+'''
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n=int(input())
+set1=set(map(int,input().split()))
+b=int(input())
+set2=set(map(int,input().split()))
+ans=set1.symmetric_difference(set2)
+print(len(ans))
