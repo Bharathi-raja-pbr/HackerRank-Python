299de82f27edf4f4d2b9bce22f6166bb4d04fdb6
Bharathi-raja-pbr <76565518+Bharathi-raja-pbr@users.noreply.github.com>
2023-07-21 12:25:13 +0530

    Create Check subset.py

---
+++ Sets/Check subset.py
@@ -0,0 +1,64 @@
+'''
+You are given two sets, A and B.
+Your job is to find whether set A is a subset of set B.
+
+If set A is subset of set B, print True.
+If set A is not a subset of set B , print False.
+
+Input Format
+
+The first line will contain the number of test cases, T.
+The first line of each test case contains the number of elements in set .
+The second line of each test case contains the space separated elements of set .
+The third line of each test case contains the number of elements in set .
+The fourth line of each test case contains the space separated elements of set .
+
+Constraints
+0<21<T
+0< no of elements in set <1001
+Output Format
+
+Output True or False for each test case on separate lines.
+
+Sample Input
+
+3
+5
+1 2 3 5 6
+9
+9 8 5 6 3 2 1 4 7
+1
+2
+5
+3 6 5 4 1
+7
+1 2 3 5 6 8 9
+3
+9 8 2
+Sample Output
+
+True 
+False
+False
+Explanation
+
+Test Case 01 Explanation
+
+Set A = {1 2 3 5 6}
+Set B = {9 8 5 6 3 2 1 4 7}
+All the elements of set A are elements of set B .
+Hence, set A is a subset of set B
+'''
+
+t=int(input())
+while(t>0):
+    n=int(input())
+    A=set(map(int,input().split()))
+    m=int(input())
+    B=set(map(int,input().split()))
+    #A.difference_update(B)
+    if len(A-B)==0:
+        print('True')
+    else :
+        print('False')
+    t-=1
